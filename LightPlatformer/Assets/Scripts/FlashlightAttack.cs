ed780b023e23bf819ab4a9e276377bace47ea096
Torfi <kacperstrzelczyk04@gmail.com>
2024-05-23 19:23:35 +0200

    Added attack cooldown
    attack cooldown
    flashlight turn off and slowly on

--- LightPlatformer/Assets/Scripts/FlashlightAttack.cs
+++ LightPlatformer/Assets/Scripts/FlashlightAttack.cs
@@ -3,49 +3,82 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
 
 public class FlashlightAttack : MonoBehaviour
 {
-    [SerializeField] GameObject light;
+    [SerializeField] Light2D Light;
+    public float normalIntensity;
+    private bool attacked = false;
 
     private List<GameObject> enemies = new List<GameObject>();
 
     public LayerMask enemyLayer;
 
+    public float attackCooldown = 3f;
+    private float cooldownTimer = 0f;
+    private float cooldownPercentage = 1f;
+    private bool canAttack = true;
+
     void Start()
     {
+        normalIntensity = Light.intensity;
     }
 
     void Update()
     {
+        if (attacked)
+        {
+            FlashlightFlash();
+        }
+
+        //Attack cooldown
+        if (!canAttack)
+        {
+            cooldownTimer += Time.deltaTime;
+            cooldownPercentage = cooldownTimer/attackCooldown;
+
+            Light.intensity = normalIntensity * cooldownPercentage;
+            if (cooldownTimer >= attackCooldown)
+            {
+                canAttack = true;
+                cooldownPercentage = 1f;
+                cooldownTimer = 0f;
+            }
+        }
     }
 
     public void Attack(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            //Sort list by enemy distance to player
-            enemies.Sort(
-                delegate (GameObject enemy1, GameObject enemy2)
-                {
-                    float distance1 = (enemy1.transform.position - transform.position).magnitude;
-                    float distance2 = (enemy2.transform.position - transform.position).magnitude;
-                    return distance1.CompareTo(distance2);
-                }
-            );
-
-          
-            List<GameObject> enemiesToDestroy = new List<GameObject>(enemies);
-            foreach (GameObject enemy in enemiesToDestroy)
+            #region Delete enemies
+            if (canAttack)
             {
-                Vector2 direction = enemy.transform.position - transform.position;
+                canAttack = false;
+                //Sort list by enemy distance to player
+                enemies.Sort(
+                    delegate (GameObject enemy1, GameObject enemy2)
+                    {
+                        float distance1 = (enemy1.transform.position - transform.position).magnitude;
+                        float distance2 = (enemy2.transform.position - transform.position).magnitude;
+                        return distance1.CompareTo(distance2);
+                    }
+                );
 
-                RaycastHit2D hit = Physics2D.Raycast(transform.position, direction.normalized, Mathf.Infinity, enemyLayer);
-                if (hit.collider.gameObject == enemy)
+                List<GameObject> enemiesToDestroy = new List<GameObject>(enemies);
+                foreach (GameObject enemy in enemiesToDestroy)
                 {
-                    DestroyEnemy(enemy);
+                    Vector2 direction = enemy.transform.position - transform.position;
+
+                    RaycastHit2D hit = Physics2D.Raycast(transform.position, direction.normalized, Mathf.Infinity, enemyLayer);
+                    if (hit.collider.gameObject == enemy)
+                    {
+                        DestroyEnemy(enemy);
+                    }
                 }
             }
+            #endregion
         }
     }
 
@@ -53,6 +86,11 @@ public class FlashlightAttack : MonoBehaviour
     {
         enemies.Remove(enemy);
         Destroy(enemy);
+    }
+
+    private void FlashlightFlash()
+    {
+        
     }
 
     //Adds object to list on collision enter, removes on collision exit
